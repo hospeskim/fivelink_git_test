038fc2fc422a56fc4ea8c7770ade143079a5a1e8
hospeskim <hospeskim@gmail.com>
2021-07-30 14:37:20 +0900

    변수 적용2

--- prac_git/src/main/java/com/prac/git/PracGitApplication.java
+++ prac_git/src/main/java/com/prac/git/PracGitApplication.java
@@ -8,6 +8,9 @@ public class PracGitApplication {
 	
 	
 	int testVal = 0;
+	int testVal2 = 10;
+	int testVal3 = 20;
+	int testVal4 = 30;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PracGitApplication.class, args);
